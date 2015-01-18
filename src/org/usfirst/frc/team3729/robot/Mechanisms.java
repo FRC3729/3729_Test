9fa10962645eb144344d534355dcbe6a7f02788f
Eraku <thatley3@comcast.net>
2015-01-18 16:14:00 -0700

    Intake code in Mechanisms

--- src/org/usfirst/frc/team3729/robot/Mechanisms.java
+++ src/org/usfirst/frc/team3729/robot/Mechanisms.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3729.robot;
 
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Mechanisms {
 	
@@ -8,10 +9,25 @@ public class Mechanisms {
 	
 	private static Mechanisms INSTANCE = null;
 	
-	private Relay testspike;
+	private Relay arm0;
+	private Relay arm1;
+	private Relay intake;
+	private Relay heightadj;
+	
+	private Talon elevator0;
+	private Talon elevator1;
+	private Talon ejector;
 	
 	private Mechanisms() {
-		testspike = new Relay(Params.port_test);
+		arm0 = new Relay(Params.port_arm0);
+		arm1 = new Relay(Params.port_arm1);
+		intake = new Relay(Params.port_intake);
+		heightadj = new Relay(Params.port_heightadj);
+		
+		elevator0 = new Talon(Params.port_elev0);
+		elevator1 = new Talon(Params.port_elev1);
+		ejector = new Talon(Params.port_eject);
+		
 		_input = new Input();
 	}
 	
@@ -23,15 +39,43 @@ public class Mechanisms {
     }
 	
 	public void test() {
-		if (_input.getButton(0, 8)) {
-        	testspike.set(Relay.Value.kForward);
-        	if (Params.testing_mech){System.out.println("Testing Forward");}
-        } else if (_input.getButton(0, 9)) {
-        	testspike.set(Relay.Value.kReverse);
-        	if (Params.testing_mech){System.out.println("Testing Reverse");}
-        }else {
-        	testspike.set(Relay.Value.kOff);
-        }
+		
 	}
-
+	
+	public void intake() {
+		if (_input.getButton(2, 6)) {
+			intake.set(Relay.Value.kReverse);
+		} else if (_input.getButton(2, 5)) {
+			intake.set(Relay.Value.kForward);
+		} else {
+			intake.set(Relay.Value.kOff);
+		}
+	}
+	public void arms() {
+		
+	}
+	public void elevator() {
+		
+	}
+	public void ejector() {
+		
+	}
+	public void height() {
+		
+	}
+	public void stop() {
+		arm0.set(Relay.Value.kOff);
+		arm1.set(Relay.Value.kOff);
+		intake.set(Relay.Value.kOff);
+		heightadj.set(Relay.Value.kOff);
+		elevator0.set(0.0);
+		elevator1.set(0.0);
+		ejector.set(0.0);
+	}
+	
+	
+	
+	
+	
+	
 }
