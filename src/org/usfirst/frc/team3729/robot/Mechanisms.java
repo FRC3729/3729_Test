c57a4da87f4ee37f53d5dca10f0478041b2ad857
Mr. Heidenry <mheidenry@regisjesuit.com>
2015-02-05 16:11:24 -0700

    pseudocode of elevator made

--- src/org/usfirst/frc/team3729/robot/Mechanisms.java
+++ src/org/usfirst/frc/team3729/robot/Mechanisms.java
@@ -3,11 +3,18 @@ package org.usfirst.frc.team3729.robot;
  
  
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType; import edu.wpi.first.wpilibj.PIDSource.PIDSourceParameter;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.PIDSource.PIDSourceParameter;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Mechanisms {
 	  private static Mechanisms INSTANCE = null;
+	  Encoder encoder1; //create instance
+	  Talon motor1 = new Talon(0);
+	 
 	  
 	  public static Mechanisms getInstance()
 	    {		  
@@ -27,8 +34,55 @@ public class Mechanisms {
 		}
 	  
 	  public void elevator(){
-			
-		}
+//		  while (buttonY isPressed)       or     while (buttonY isPressed) &&  (encoder.get() != top) 
+//		  {
+//			motor.moveForward();
+//		  		if (encoder.get() == top)
+//		  			motor.stop();
+//				  
+//		  }
+		  
+//		  while (buttonA isPressed)       
+//		  {
+//			motor.moveBackwards();
+//		  		if (encoder.get() == bottom)
+//		  			motor.stop();
+//				  
+//		  }
+//		  
+//		  
+//		  if (backbutton.isPressed())
+//		  {
+//		  		while(encoder.get() > 0)
+//		  		{
+//		  		motor.reverse();
+//	  			}
+//	  	  }
+//		  
+//		  if (input.getPOV == 0)
+//		  {
+//			  desiredPosition = encoder.getCurrentPosition() + 30;
+//			  	
+//			  while (encoder.getCurrentPosition() < desiredPosition)
+//			  {
+//				  motor.forward();
+//			  }
+//		  }
+//		  
+//		  if (input.getPOV == 180)
+//		  {
+//		  desiredPosition = encoder.getCurrentPosition() - 30;
+//		  	
+//		  while (encoder.getCurrentPosition() > desiredPosition)
+//		  {
+//			  motor.reverse();
+//		  }
+//	  }
+//
+//
+//		  
+			}
+		  
 	  
 	  public void test(){
 		  
